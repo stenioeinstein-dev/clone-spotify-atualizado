3894d324b915da001cf3e14195ac55f7ea424c0c
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-27 20:55:30 -0300

    Left side creation

---
+++ src/components/left-h/Lefth.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function LeftH() {
+  return (
+    <div className={`${styles.container}`} >
+left
+    </div>
+  );
+}
