09612d41868cf96eea34f4a0124a1c5a89bebb1a
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-28 19:09:35 -0300

    Add autofocus

--- src/components/inp/Floatinginput.js
+++ src/components/inp/Floatinginput.js
@@ -50,7 +50,7 @@ export default function FloatingInput() {
                                 <path d="M7 1.75a5.25 5.25 0 1 0 0 10.5 5.25 5.25 0 0 0 0-10.5zM.25 7a6.75 6.75 0 1 1 12.096 4.12l3.184 3.185a.75.75 0 1 1-1.06 1.06L11.304 12.2A6.75 6.75 0 0 1 .25 7z"></path>
                             </svg>
                         </div>
-                        <input type="text" placeholder="What do you want to listen to?" id="search" />
+                        <input type="text" autoFocus placeholder="What do you want to listen to?" id="search" />
                     </form>
                 </div>
                 <div className="searchresult">
