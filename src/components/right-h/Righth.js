fc69365c610ff50783159afa8643fc693f5045e3
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-28 13:18:44 -0300

    Change itens and add footer

--- src/components/right-h/Righth.js
+++ src/components/right-h/Righth.js
@@ -1,4 +1,5 @@
-import Itens from "@/components/itens/Itens";
+import Playlists from "@/components/itens/playlists/Playlists";
+import Artists from "@/components/itens/artists/Artists";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Footer from "./footer/footer";
@@ -6,16 +7,16 @@ import Footer from "./footer/footer";
 export default function RightH() {
   return (
     <div className={styles.container}>
-      <div className={styles.playlist}>
-        <h1 id="greeting">{/*{getGreeting()} */}Boas vindas</h1>
+      {/* <div className={styles.playlist}>
+        <h1 id="greeting">{getGreeting()} Boas vindas</h1>
         <h2 className="session">Navegar por todas as seções</h2>
+      </div> */}
+      <div className={styles.content}>
+          <Artists />
+          <Playlists/>
       </div>
-      <div className={styles.content_itens}>
-        <Itens />
-        
-      </div>
-      <iframe className="ml-2 mr-2" src="https://open.spotify.com/embed/track/5oI9blwsXC8AkTWw6wASY8?utm_source=generator&theme=0" width="98.5%"  height="152" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <Footer/>
+      <iframe className="ml-2 mr-2" src="https://open.spotify.com/embed/track/5oI9blwsXC8AkTWw6wASY8?utm_source=generator&theme=0" width="98.5%" height="152" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <Footer />
     </div>
   );
 }
