115c35d19cdd94206fea2ed7b6e95562cac499ba
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-28 19:41:45 -0300

    Update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -91,18 +91,18 @@ export default function Header() {
                                     /
                                 </div>
                                 <div className={styles.icon_browser} onClick={toggleButton} title="Browser">
-                                {isSelected ? (
-                                    // SVG quando o botão está selecionado
-                                    <svg data-encore-id="icon" role="img" aria-hidden="true" className="Svg-sc-ytk21e-0 bneLcE e-9541-icon" viewBox="0 0 24 24">
-                                        <path d="M4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4H4V2zM1.513 9.37A1 1 0 0 1 2.291 9H21.71a1 1 0 0 1 .978 1.208l-2.17 10.208A2 2 0 0 1 18.562 22H5.438a2 2 0 0 1-1.956-1.584l-2.17-10.208a1 1 0 0 1 .201-.837zM12 17.834c1.933 0 3.5-1.044 3.5-2.333 0-1.289-1.567-2.333-3.5-2.333S8.5 14.21 8.5 15.5c0 1.289 1.567 2.333 3.5 2.333z"></path>
-                                    </svg>
-                                ) : (
-                                    // SVG quando o botão não está selecionado
-                                    <svg data-encore-id="icon" role="img" aria-hidden="true" className="Svg-sc-ytk21e-0 bneLcE e-9541-icon" viewBox="0 0 24 24">
-                                        <path d="M15 15.5c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2z" ></path>
-                                        <path d="M1.513 9.37A1 1 0 0 1 2.291 9h19.418a1 1 0 0 1 .979 1.208l-2.339 11a1 1 0 0 1-.978.792H4.63a1 1 0 0 1-.978-.792l-2.339-11a1 1 0 0 1 .201-.837zM3.525 11l1.913 9h13.123l1.913-9H3.525zM4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4h-2V3H6v3H4V2z"></path>
-                                    </svg>
-                                )}
+                                    {isSelected ? (
+                                        // SVG quando o botão está selecionado
+                                        <svg data-encore-id="icon" role="img" aria-hidden="true" className="Svg-sc-ytk21e-0 bneLcE e-9541-icon" viewBox="0 0 24 24">
+                                            <path d="M4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4H4V2zM1.513 9.37A1 1 0 0 1 2.291 9H21.71a1 1 0 0 1 .978 1.208l-2.17 10.208A2 2 0 0 1 18.562 22H5.438a2 2 0 0 1-1.956-1.584l-2.17-10.208a1 1 0 0 1 .201-.837zM12 17.834c1.933 0 3.5-1.044 3.5-2.333 0-1.289-1.567-2.333-3.5-2.333S8.5 14.21 8.5 15.5c0 1.289 1.567 2.333 3.5 2.333z"></path>
+                                        </svg>
+                                    ) : (
+                                        // SVG quando o botão não está selecionado
+                                        <svg data-encore-id="icon" role="img" aria-hidden="true" className="Svg-sc-ytk21e-0 bneLcE e-9541-icon" viewBox="0 0 24 24">
+                                            <path d="M15 15.5c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2z" ></path>
+                                            <path d="M1.513 9.37A1 1 0 0 1 2.291 9h19.418a1 1 0 0 1 .979 1.208l-2.339 11a1 1 0 0 1-.978.792H4.63a1 1 0 0 1-.978-.792l-2.339-11a1 1 0 0 1 .201-.837zM3.525 11l1.913 9h13.123l1.913-9H3.525zM4 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4h-2V3H6v3H4V2z"></path>
+                                        </svg>
+                                    )}
                                 </div>
                             </form>
                         </div>
@@ -126,25 +126,25 @@ export default function Header() {
                         </button>
                         {/* Links do menu */}
                         <nav className={`${styles.modal_mobile_nav} flex flex-col space-y-6 text-left`}>
-                            <a href="#home" className={`${styles.modal_mobile_nav_nm} hover:underline`}>
+                            <Link href="#login" className={`${styles.modal_mobile_nav_nm} hover:underline`}>
                                 Log in
-                            </a>
-                            <a href="#home" className={`${styles.modal_mobile_nav_nm} hover:underline`}>
+                            </Link>
+                            <Link href="#signup" className={`${styles.modal_mobile_nav_nm} hover:underline`}>
                                 Sign up
-                            </a>
+                            </Link>
                             <hr className={styles.hr} />
-                            <a href="#services" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
+                            <Link href="#premium" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
                                 Premium
-                            </a>
-                            <a href="#services" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
+                            </Link>
+                            <Link href="#help" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
                                 Help
-                            </a>
-                            <a href="#services" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
+                            </Link>
+                            <Link href="#download" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
                                 Download
-                            </a>
-                            <a href="#services" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
+                            </Link>
+                            <Link href="#terms" className={`${styles.modal_mobile_nav_sub} hover:underline `}>
                                 Terms
-                            </a>
+                            </Link>
                         </nav>
                     </div>
                 )}
