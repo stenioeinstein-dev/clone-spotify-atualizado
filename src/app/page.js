61314e23f59d9eae87c3ffaf8947cb0a67c46bfa
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-28 13:14:23 -0300

    Add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
+import Footer from "@/components/footer/Footer";
 import styles from "./page.module.css";
 import LeftH from "@/components/left-h/Lefth";
 import RightH from "@/components/right-h/Righth";
 import Image from "next/image";
 
 export default function Home() {
-  return (
+  return (<>
     <div className={styles.container}>
       <LeftH />
       <RightH />
     </div>
+    <Footer />
+  </>
   );
 }
