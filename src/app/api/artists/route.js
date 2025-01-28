3b95dee0246c8524dd50ae22881db7e90ff79fed
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-28 19:06:45 -0300

    Update

--- src/app/api/artists/route.js
+++ src/app/api/artists/route.js
@@ -3,9 +3,21 @@ export async function GET(req, res) {
         [
             {
                 "id": 1,
-                "name": "Boas festas",
+                "name": "Artist 1",
                 "urlartist": "/",
                 "urlImg": "/1.jpeg"
+            },
+            {
+                "id": 2,
+                "name": "Artist 2",
+                "urlartist": "/",
+                "urlImg": "/2.jpeg"
+            },
+            {
+                "id": 3,
+                "name": "Artist 3",
+                "urlartist": "/",
+                "urlImg": "/3.jpeg"
             },
             
         ]
