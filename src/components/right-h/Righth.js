c907a8e64d15f77c91bbd6a770bd12913791848f
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-01-29 12:26:11 -0300

    Update Righth.js

--- src/components/right-h/Righth.js
+++ src/components/right-h/Righth.js
@@ -7,10 +7,6 @@ import Footer from "./footer/footer";
 export default function RightH() {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.playlist}>
-        <h1 id="greeting">{getGreeting()} Boas vindas</h1>
-        <h2 className="session">Navegar por todas as seções</h2>
-      </div> */}
       <div className={styles.content}>
           <Artists />
           <Playlists/>
