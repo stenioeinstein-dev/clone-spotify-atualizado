12bc6c92e2bebf3549396e3a303ad9b4a7237bef
Stênio <109425215+stenioeinstein-dev@users.noreply.github.com>
2025-05-20 10:22:28 -0300

    Atualizar o route.js

--- src/app/api/artists/route.js
+++ src/app/api/artists/route.js
@@ -5,21 +5,21 @@ export async function GET(req, res) {
                 "id": 40,
                 "name": "Artist 1",
                 "s": "Artist",
-                "urlartist": "/",
+                "urlartist": "#art1",
                 "urlImg": "/1.jpeg"
             },
             {
                 "id": 41,
                 "name": "Artist 2",
                 "s": "Artist",
-                "urlartist": "/",
+                "urlartist": "#art2",
                 "urlImg": "/2.jpeg"
             },
             {
                 "id": 42,
                 "name": "Artist 3",
                 "s": "Artist",
-                "urlartist": "/",
+                "urlartist": "#art3",
                 "urlImg": "/3.jpeg"
             },
             
